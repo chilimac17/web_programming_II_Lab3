7d0ee9ea9ad8d88e13f8298c33585e0e460d3ee3
Michael Chillemi <michaelchillemi17@gmail.com>
2026-02-23 23:29:13 -0500

    fixing functions have some done

--- resolvers.js
+++ resolvers.js
@@ -13,6 +13,39 @@ export const resolvers = {
     Query: {
         artists: async (_, args) => {
             const artists_collection = await artistsCollection();
+            const all_artists = await artists_collection.find({}).toArray();
+            if (!all_artists) {
+                throw new GraphQLError("Artist not found", {
+                    extensions: { code: "ARTIST_NOT_FOUND" }
+                });
+            }
+            return all_artists;
+        },
+
+        albums: async (_, args) => {
+const albums_collection = await albumsCollection();
+            const all_albums = await albums_collection.find({}).toArray();
+            if (!all_albums) {
+                throw new GraphQLError("Album not found", {
+                    extensions: { code: "ALBUM_NOT_FOUND" }
+                });
+            }
+            return all_albums;
+        },
+
+        listeners: async (_, args) => {
+const listeners_collection = await listenersCollection();
+            const all_listeners = await listeners_collection.find({}).toArray();
+            if (!all_listeners) {
+                throw new GraphQLError("Listener not found", {
+                    extensions: { code: "LISTENER_NOT_FOUND" }
+                });
+            }
+            return all_listeners;
+        },
+
+        getArtistById: async (_,  args) => {
+const artists_collection = await artistsCollection();
             const artists = await artists_collection.findOne({_id: args._id});
             if (!artists) {
                 throw new GraphQLError("Artist not found", {
@@ -22,18 +55,7 @@ export const resolvers = {
             return artists;
         },
 
-        albums: async (_, args) => {
-const albums_collection = await albumsCollection();
-            const albums = await albums_collection.findOne({_id: args._id});
-            if (!albums) {
-                throw new GraphQLError("Album not found", {
-                    extensions: { code: "ALBUM_NOT_FOUND" }
-                });
-            }
-            return albums;
-        },
-
-        listeners: async (_, args) => {
+        getListenerById: async (_,  args) => {
 const listeners_collection = await listenersCollection();
             const listeners = await listeners_collection.findOne({_id: args._id});
             if (!listeners) {
@@ -44,16 +66,16 @@ const listeners_collection = await listenersCollection();
             return listeners;
         },
 
-        getArtistById: async (_,  args) => {
-
-        },
-
-        getListenerById: async (_,  args) => {
-
-        },
-
 
 getAlbumById: async (_,  args) => {
+    const albums_collection = await albumsCollection();
+            const albums = await albums_collection.findOne({_id: args._id});
+            if (!albums) {
+                throw new GraphQLError("Album not found", {
+                    extensions: { code: "ALBUM_NOT_FOUND" }
+                });
+            }
+            return albums;
 
         },
 
